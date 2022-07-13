fb87a7523e08e5b901335db93504662acfe6d03c
Kaleniuk <kaleniuk7@yahoo.com.br>
2022-07-13 20:05:31 -0300

    App Repositories - DIO

---
+++ app/src/main/java/me/dio/app_repositories/App.kt
@@ -0,0 +1,16 @@
+package me.dio.app_repositories
+
+import android.app.Application
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+//        starKoin {
+//
+//
+//        }
+    }
+
+}
---
+++ app/src/main/java/me/dio/app_repositories/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.dio.app_repositories.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.app_repositories.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
